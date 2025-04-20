94f58d4bf1ad9e90427c951d551ca22a0afb109f
Victor Augusto Barbosa Moreno <victor.moreno@simova.com.br>
2025-04-20 19:38:07 -0300

    chore(setup): add Toaster provider to main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import 'leaflet/dist/leaflet.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
+import { Toaster } from 'react-hot-toast';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+    <Toaster position="top-right" toastOptions={{ duration: 3000 }} />
+  </React.StrictMode>
+);
